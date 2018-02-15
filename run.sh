6c30cac7bfbd4fb74a52e8dd1cc75cb7af76d31e
Lorenzo Biava <lorenzo-biava@users.noreply.github.com>
2018-02-15 11:46:01 +0100

    Add Scenario file download in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ LOCUST_CMD="/usr/local/bin/locust"
 LOCUST_OPTS="-f $SCENARIO_FILE --host=$TARGET_URL"
 LOCUST_MODE=${LOCUST_MODE:-standalone}
 
+if [ -z "$SCENARIO_FILE_URL" ]; then
+  echo "=> Downloading scenario file from $SCENARIO_FILE_URL"
+  curl -o $SCENARIO_FILE $SCENARIO_FILE_URL
+fi
+
 if [ "$LOCUST_MODE" = "master" ]; then
   LOCUST_OPTS="$LOCUST_OPTS --master"
 elif [ "$LOCUST_MODE" = "slave" ]; then
